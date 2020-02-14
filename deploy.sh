3d8cf9fd66c59e27415bf5a9141ba64792d8167d
Aicirou <2643053021@qq.com>
2020-02-14 22:51:15 +0800

    部署脚本优化

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
-echo("test")
-cd /data/hexoblog
+BLOG_PATH="/data/hexoblog"
+echo "======================================"
+echo "script: cd $BLOG_PATH"
+cd $BLOG_PATH
+echo "script: git pull..."
 git pull
+echo "script: hexo clean..."
 hexo clean
+echo "script: hexo g..."
 hexo g
