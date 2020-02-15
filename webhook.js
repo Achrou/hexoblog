11c6a98ac1f7e867f1289a3472a6b981f6336cf4
Aicirou <2643053021@qq.com>
2020-02-15 18:21:24 +0800

    调整webhooks为gitee

--- webhook.js
+++ webhook.js
@@ -1,7 +1,8 @@
 var http = require('http')
-var createHandler = require('github-webhook-handler')
+// var createHandler = require('github-webhook-handler')
+var createHandler = require('gitee-webhook-handler')
 var handler = createHandler({ path: '/', secret: 'momik_blog' })
-// 上面的 secret 保持和 GitHub 后台设置的一致
+
 function run_cmd(cmd, args, callback) {
   var spawn = require('child_process').spawn;
   var child = spawn(cmd, args);
@@ -9,18 +10,31 @@ function run_cmd(cmd, args, callback) {
   child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
   child.stdout.on('end', function() { callback (resp) });
 }
-http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
-  })
-}).listen(8811)
 handler.on('error', function (err) {
   console.error('Error:', err.message)
 })
-handler.on('push', function (event) {
+// Gitee
+handler.on('Push Hook', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref);
+    run_cmd('sh', ['./deploy.sh'], function(text){ console.log(text) });
+})
+// GitHub
+/*handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref);
     run_cmd('sh', ['./deploy.sh',event.payload.repository.name], function(text){ console.log(text) });
-})
+})*/
+try {
+  http.createServer(function (req, res) {
+    handler(req, res, function (err) {
+      res.statusCode = 404
+      res.end('no such location')
+    })
+  }).listen(8811)
+}catch(err){
+  console.error('Error:', err.message)
+}
+
